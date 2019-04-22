98bd5894a11a433a594775f220253416f0b38598
superanika <anamaria.romerogarcia@gmail.com>
2019-04-22 11:39:45 +0200

    set fav function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,45 +1,55 @@
 'use strict';
 
-const url = 'http://api.tvmaze.com/search/shows?q=';
-const input = document.querySelector('.input');
 const button = document.querySelector('.button');
 const results = document.querySelector('.results');
 
-function getFilms(){
+function getSeries(){
+  const url = 'http://api.tvmaze.com/search/shows?q=';
+  const input = document.querySelector('.input');
   const inputValue = input.value;
   fetch(url + inputValue)
     .then( response => response.json())
     .then (data => {
       createList(data);
+      favorite();
     });
 }
 
 function createList(data){
   for (const item of data) {
-    const films =item.show;
-    const name = films.name;
-    const image = films.image.medium;
-    const listItem =document.createElement('li');
+    const series =item.show;
+    const name = series.name;
+    const image = series.image;
+    const listItem = document.createElement('li');
     listItem.classList.add('list__item');
     results.appendChild(listItem);
-    const filmName = document.createElement('h2');
-    filmName.classList.add('film__title');
+    const serieName = document.createElement('h2');
+    serieName.classList.add('serie__title');
     const titleContent = document.createTextNode(name);
-    filmName.appendChild(titleContent);
-    listItem.appendChild(filmName);
-    const filmPreview = document.createElement('img');
-    filmPreview.classList.add('film_img');
-    filmPreview.src = image;
-    listItem.appendChild(filmPreview);
+    serieName.appendChild(titleContent);
+    listItem.appendChild(serieName);
+    const seriePreview = document.createElement('img');
+    seriePreview.classList.add('serie_img');
+    if (image === null) {
+      seriePreview.src = 'https://via.placeholder.com/210x295/ffffff/6666ff/?text=TV';
+    }else {
+      seriePreview.src = image.medium;
+    }
+    listItem.appendChild(seriePreview); 
+  }
+}
+function favorite() {
+  const allSeries = document.querySelectorAll('.list__item');
+  for (const item of allSeries) {
+    item.addEventListener('click', function(){item.classList.toggle('fav');});
   }
 }
 
-
-
-function searchFilms() {
+function searchSeries() {
   results.innerHTML = '';
-  getFilms();
+  getSeries();
+ 
 }
 
-button.addEventListener('click',searchFilms);
+button.addEventListener('click',searchSeries);
 
