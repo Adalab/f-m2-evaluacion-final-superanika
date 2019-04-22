7c50e30043272ca9b5b928e41c74aaea6a844d46
superanika <anamaria.romerogarcia@gmail.com>
2019-04-22 18:30:26 +0200

    fix fav list and set localStorage

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,6 +4,7 @@ const button = document.querySelector('.button');
 const results = document.querySelector('.results');
 const favSeriesList = document.querySelector('.fav__series');
 
+//obtenemos los datos de la api
 function getSeries(){
   const url = 'http://api.tvmaze.com/search/shows?q=';
   const input = document.querySelector('.input');
@@ -11,8 +12,8 @@ function getSeries(){
   fetch(url + inputValue)
     .then( response => response.json())
     .then (data => {
-      createList(data);
-      fav();
+      createList(data); //creamos la lista
+      fav(); //añadimos la clase fav y creamos la lista de favoritos
     });
 }
 
@@ -31,6 +32,7 @@ function createList(data){
     listItem.appendChild(serieName);
     const seriePreview = document.createElement('img');
     seriePreview.classList.add('serie_img');
+    //si el servidor no devuelve imagen, ponemos una por defecto
     if (image === null) {
       seriePreview.src = 'https://via.placeholder.com/210x295/ffffff/6666ff/?text=TV';
     }else {
@@ -39,36 +41,56 @@ function createList(data){
     listItem.appendChild(seriePreview);
   }
 }
+//creamos un objeto vacio para luego almacenar los datos de la serie favorita
+
+
 function fav() {
   const allSeries = document.querySelectorAll('.list__item');
-  const favSeries = [];
   const favDiv = document.querySelector('.fav__list');
-
+  let infoFav = {
+    title: '',
+    image: ''
+  };
   for (const item of allSeries) {
     // eslint-disable-next-line no-inner-declarations
     function favList() {
+      //añade la clase favorita
       item.classList.add('fav');
-      //if(item.classList.contains('fav')){
-      favSeries.push(item);
-      if (favSeries.length !== 0){
+      infoFav.title = item.firstChild.innerHTML;
+      infoFav.image = item.lastChild.src;
+      //pintamos la lista de favoritos
+      if (infoFav.title !== ''){
         favDiv.classList.remove('hidden');
-        for (const favItem of favSeries) {
-          favSeriesList.appendChild(favItem);
+        const favItem = document.createElement('li');
+        favItem.classList.add('fav__list-item');
+        const favTitle = document.createElement('h3');
+        favTitle.classList.add('fav__title');
+        const favTitleContent = document.createTextNode(infoFav.title);
+        const favImg = document.createElement('img');
+        favImg.classList.add('fav_img');
+        favImg.src = infoFav.image;
+        favTitle.appendChild(favTitleContent);
+        favItem.appendChild(favTitle);
+        favItem.appendChild(favImg);
+        favSeriesList.appendChild(favItem);
+
+        let savedFav = JSON.parse(localStorage.getItem('savedFav'));
+
+        if (savedFav !== null) {
+          savedFav.push(infoFav);
+        }else {
+          savedFav = [];
+          savedFav.push(infoFav);
         }
+        localStorage.setItem('savedFav', JSON.stringify(savedFav));
+
       }else {
         favDiv.classList.add('hidden');
-      }
-      const allFavs = document.querySelectorAll('.fav');
-      for (const favs of allFavs) {
-        localStorage.setItem('savedFav', JSON.stringify(favs));
       }
     }
     item.addEventListener('click', favList);
   }
 }
-
-
-
 
 
 function searchSeries() {
@@ -77,7 +99,7 @@ function searchSeries() {
 
 }
 
-//JSON.parse(localStorage.getItem('savedFav'));
+JSON.parse(localStorage.getItem('savedFav'));
 button.addEventListener('click',searchSeries);
 
 
