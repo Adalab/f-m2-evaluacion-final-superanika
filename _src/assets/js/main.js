6250501fb937012e749795bbf52c99973948d256
superanika <anamaria.romerogarcia@gmail.com>
2019-04-23 11:24:20 +0200

    gulp docs and create delete icon

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,7 +7,6 @@ const favDiv = document.querySelector('.fav__list');
 const main = document.querySelector('.main');
 const noResults = document.querySelector('.no__results');
 
-//obtenemos los datos de la api
 function getSeries(){
   const url = 'http://api.tvmaze.com/search/shows?q=';
   const input = document.querySelector('.input');
@@ -19,7 +18,7 @@ function getSeries(){
       if (data.length === 0){
         errorMsg();
       }else {
-        createList(data); //creamos la lista
+        createList(data);
       }
     });
 }
@@ -48,7 +47,6 @@ function createList(data){
     listItem.appendChild(serieName);
     const seriePreview = document.createElement('img');
     seriePreview.classList.add('serie_img');
-    //si el servidor no devuelve imagen, ponemos una por defecto
     if (image === null) {
       seriePreview.src = 'https://via.placeholder.com/210x295/ffffff/6666ff/?text=TV';
     }else {
@@ -58,40 +56,41 @@ function createList(data){
   }
 }
 
-// Pintar el listado de favoritos en pantalla
+function createFavs(array) {
+  const favItem = document.createElement('li');
+  favItem.classList.add('fav__list-item');
+  const icon = document.createElement('i');
+  icon.classList.add('fas');
+  icon.classList.add('fa-times-circle');
+  const favTitle = document.createElement('h3');
+  favTitle.classList.add('fav__series-title');
+  const favTitleContent = document.createTextNode(array.title);
+  const favImg = document.createElement('img');
+  favImg.classList.add('fav_img');
+  favImg.src = array.image;
+  favTitle.appendChild(favTitleContent);
+  favItem.appendChild(icon);
+  favItem.appendChild(favTitle);
+  favItem.appendChild(favImg);
+  favSeriesList.appendChild(favItem);
+}
+
 function showSavedFavorites() {
-  // Recuperar los favoritos
   const savedFav = JSON.parse(localStorage.getItem('savedFav'));
 
-  // Pintar titulo de series favoritas si hay alguna en favoritos
   if (savedFav !== null && savedFav.length > 0) {
     favDiv.classList.remove('hidden');
-    // Pintamos el listado de series favoritas
     for (const item of savedFav) {
-      const favItem = document.createElement('li');
-      favItem.classList.add('fav__list-item');
-      const favTitle = document.createElement('h3');
-      favTitle.classList.add('fav__title');
-      const favTitleContent = document.createTextNode(item.title);
-      const favImg = document.createElement('img');
-      favImg.classList.add('fav_img');
-      favImg.src = item.image;
-      favTitle.appendChild(favTitleContent);
-      favItem.appendChild(favTitle);
-      favItem.appendChild(favImg);
-      favSeriesList.appendChild(favItem);
+      createFavs(item);
     }
   } else{
     favDiv.classList.add('hidden');
   }
 }
 
-// Pone la clase de favorito al item y añadirlo a localstorage
 function addFavorite(event) {
-  // Añadir la clase favorito
   event.currentTarget.classList.add('fav');
 
-  // Recuperar informacion item
   let infoFav = {
     title: '',
     image: ''
@@ -100,7 +99,6 @@ function addFavorite(event) {
   infoFav.title = event.currentTarget.firstChild.innerHTML;
   infoFav.image = event.currentTarget.lastChild.src;
 
-  // Recuperar favoritos que tengo guardado en localstorage
   let savedFav = JSON.parse(localStorage.getItem('savedFav'));
   if (savedFav === null) {
     savedFav = [];
@@ -109,21 +107,8 @@ function addFavorite(event) {
     savedFav.push(infoFav);
     localStorage.setItem('savedFav', JSON.stringify(savedFav));
 
-    // Crear el favorito en pantalla
-    const favItem = document.createElement('li');
-    favItem.classList.add('fav__list-item');
-    const favTitle = document.createElement('h3');
-    favTitle.classList.add('fav__title');
-    const favTitleContent = document.createTextNode(infoFav.title);
-    const favImg = document.createElement('img');
-    favImg.classList.add('fav_img');
-    favImg.src = infoFav.image;
-    favTitle.appendChild(favTitleContent);
-    favItem.appendChild(favTitle);
-    favItem.appendChild(favImg);
-    favSeriesList.appendChild(favItem);
+    createFavs(infoFav);
   }
-
 }
 
 function alreadyExist(savedFav, infoFav) {
